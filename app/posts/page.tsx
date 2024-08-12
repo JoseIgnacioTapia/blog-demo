8c1562eb8c7b65ca82e59406c1c12234fcda098a
JoseIgnacioTapia <josignaciot@gmail.com>
2024-08-12 17:44:36 -0300

    Get all posts

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,42 +1,10 @@
 import AllPosts from '@/components/posts/AllPosts';
-
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-with-nextjs',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is a the React framework for production - it makes building fullstack React applications and sites a breeze and ships with built-in SSR.',
-    date: '2022-02-10',
-  },
-  {
-    slug: 'getting-started-with-nextjs2',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is a the React framework for production - it makes building fullstack React application',
-    date: '2022-02-10',
-  },
-  {
-    slug: 'getting-started-with-nextjs3',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is a the React framework for production - it makes building fullstack React application',
-    date: '2022-02-10',
-  },
-  {
-    slug: 'getting-started-with-nextjs4',
-    title: 'Getting Started with NextJS',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'NextJS is a the React framework for production - it makes building fullstack React application',
-    date: '2022-02-10',
-  },
-];
+import { getAllPosts } from '@/lib/posts-util';
 
 function AllPostsPage() {
-  return <AllPosts posts={DUMMY_POSTS} />;
+  const allPosts = getAllPosts();
+
+  return <AllPosts posts={allPosts} />;
 }
 
 export default AllPostsPage;
