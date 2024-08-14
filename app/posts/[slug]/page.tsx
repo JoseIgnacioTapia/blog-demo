cbd2fcc8e53ceed329006bb3d60cdb2ead843165
Hugo Avila <hugok2k@gmail.com>
2024-08-14 11:50:08 -0300

    chore: Fix image file name and update import paths

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,16 +1,14 @@
-import PostContent from '@/components/posts/post-detail/PostContent';
-import { getPostData } from '@/lib/posts-util';
+import PostContent from '@/components/posts/post-detail/PostContent'
+import { getPostData } from '@/lib/posts-util'
 
 function PostDetailPage({ params }: { params: { slug: string } }) {
-  const postContent = getPostData(params.slug + '.md');
-  console.log('post obtenido desde PostDetailPage', postContent);
-
+  const postContent = getPostData(params.slug + '.md')
   if (!postContent) {
     // Se puede redirigir, mostrar un mensaje de error o manejar el caso de que no se encuentre el post
-    return <p>Post not found.</p>;
+    return <p>Post not found.</p>
   }
 
-  return <PostContent post={postContent} />;
+  return <PostContent post={postContent} />
 }
 
-export default PostDetailPage;
+export default PostDetailPage
--- components/posts/post-detail/PostContent.tsx
+++ components/posts/post-detail/PostContent.tsx
@@ -1,37 +1,28 @@
-'use client';
-import ReactMarkdown from 'react-markdown';
-import Image from 'next/image';
-import PostHeader from './PostHeader';
-import classes from './post-content.module.css';
-import { Post } from '@/types';
+import ReactMarkdown from 'react-markdown'
+import Image from 'next/image'
+import PostHeader from './PostHeader'
+import classes from './post-content.module.css'
+import { Post } from '@/types'
 
 interface PostContentProps {
-  post: Post;
+  post: Post
 }
 
 function PostContent({ post }: PostContentProps) {
-  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`
 
   const customRenderers = {
-    image(image) {
-      console.log('ruta de la imagen');
-      return (
-        <Image
-          src={`/images/posts/${post.slug}/${image.src}`}
-          alt={image.alt}
-          width={600}
-          height={300}
-        />
-      );
-    },
-  };
+    img(image: any) {
+      return <Image src={`/images/posts/${post.slug}/${image.src}`} alt={image.alt} width={600} height={300} />
+    }
+  }
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
       <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
-  );
+  )
 }
 
-export default PostContent;
+export default PostContent
