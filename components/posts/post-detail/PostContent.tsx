95428592030c1d68ac0c0436847fb81953466b08
JoseIgnacioTapia <josignaciot@gmail.com>
2024-08-14 17:31:44 -0300

    Add code snippet

--- components/posts/post-detail/PostContent.tsx
+++ components/posts/post-detail/PostContent.tsx
@@ -1,28 +1,75 @@
-import ReactMarkdown from 'react-markdown'
-import Image from 'next/image'
-import PostHeader from './PostHeader'
-import classes from './post-content.module.css'
-import { Post } from '@/types'
+import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import PostHeader from './PostHeader';
+import classes from './post-content.module.css';
+import { Post } from '@/types';
 
 interface PostContentProps {
-  post: Post
+  post: Post;
 }
 
 function PostContent({ post }: PostContentProps) {
-  const imagePath = `/images/posts/${post.slug}/${post.image}`
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   const customRenderers = {
     img(image: any) {
-      return <Image src={`/images/posts/${post.slug}/${image.src}`} alt={image.alt} width={600} height={300} />
-    }
-  }
+      return (
+        <Image
+          src={`/images/posts/${post.slug}/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    },
+    // p(paragraph: any) {
+    //   const { node } = paragraph;
+
+    //   if (node.children[0].type === 'image') {
+    //     const image = node.children[0];
+
+    //     return (
+    //       <div className={classes.image}>
+    //         <Image
+    //           src={`/images/posts/${post.slug}/${image.url}`} // Because image is inside paragraph so use url instead src
+    //           alt={image.alt}
+    //           width={600}
+    //           height={300}
+    //         />
+    //       </div>
+    //     );
+    //   }
+
+    //   return <p>{paragraph.children}</p>;
+    // },
+
+    code({ node, inline, className, children, ...props }: any) {
+      const match = /language-(\w+)/.exec(className || '');
+      return !inline && match ? (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={match[1]}
+          PreTag='div'
+          {...props}
+        >
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+      ) : (
+        <code className={className} {...props}>
+          {children}
+        </code>
+      );
+    },
+  };
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
       <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
-  )
+  );
 }
 
-export default PostContent
+export default PostContent;
